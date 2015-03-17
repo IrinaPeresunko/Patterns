df3a042249bfef84d3d11ad40321963ebd651a08
IrinaPeresunko <peresunkoi@gmail.com>
2015-03-17 21:07:33 +0200

    pattern Observer

---
+++ Patterns/src/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,31 @@
+package observer;
+	
+public class CurrentConditionsDisplay implements Observer, DisplayElement {
+	private float temperature;
+	private float humidity;
+	private Subject weatherData;
+	
+	public CurrentConditionsDisplay(Subject weatherData) {
+		this.setWeatherData(weatherData);
+		weatherData.registerObserver(this);
+	}
+	
+	public void update(float temperature, float humidity, float pressure) {
+		this.temperature = temperature;
+		this.humidity = humidity;
+		display();
+	}
+	
+	public void display() {
+		System.out.println("Current conditions: " + temperature 
+			+ "F degrees and " + humidity + "% humidity");
+	}
+
+	public Subject getWeatherData() {
+		return weatherData;
+	}
+
+	public void setWeatherData(Subject weatherData) {
+		this.weatherData = weatherData;
+	}
+}
