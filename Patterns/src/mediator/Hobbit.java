bf81d73955a473895a21315f286357f0f893ab90
IrinaPeresunko <peresunkoi@gmail.com>
2015-04-02 22:55:13 +0300

    Pattern Mediator

---
+++ Patterns/src/mediator/Hobbit.java
@@ -0,0 +1,15 @@
+package mediator;
+
+/**
+ * 
+ * Hobbit party member.
+ *
+ */
+public class Hobbit extends PartyMemberBase {
+
+	@Override
+	public String toString() {
+		return "Hobbit";
+	}
+
+}
---
+++ Patterns/src/mediator/Party.java
@@ -0,0 +1,13 @@
+package mediator;
+/**
+ * 
+ * Party interface.
+ * 
+ */
+public interface Party {
+
+	void addMember(PartyMember member);
+
+	void act(PartyMember actor, Action action);
+
+}
